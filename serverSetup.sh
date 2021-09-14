a88c370abb45866997881911540933c294c440b8
Rodrigo Álvarez <incognia@gmail.com>
2021-09-14 06:21:50 -0500

    Update serverSetup.sh

--- serverSetup.sh
+++ serverSetup.sh
@@ -17,21 +17,26 @@ if is_user_root; then
     aptitude safe-upgrade -y
     apt install -y neofetch figlet nmon htop glances mc openssh-server ubuntu-restricted-extras
     apt install -y gnome-tweak-tool chrome-gnome-shell
-    echo 'Descargando papel tapiz'
+    echo 'Descargando papel tapiz...'
     wget https://github.com/FONDESO/Soporte/raw/main/cdmx-01.jpg
     mv cdmx-01.jpg /usr/share/backgrounds/
-    echo 'Reconfigurando GDM'
+    echo 'Reconfigurando GDM...'
     sleep 2
     update-alternatives --set gdm3-theme.gresource /usr/share/gnome-shell/gnome-shell-theme.gresource
     echo 'Deshabilitando inicio automático del ambiente gráfico...'
     systemctl set-default multi-user.target
-    echo 'Para arrancarente el ambiente gráfico use:'
+    echo 'Para arrancar nuevamente el ambiente gráfico use:'
     echo 'sudo service gdm3 start'
     sleep 2
+    echo ' _____ ___  _   _ ____  _____ ____   ___  '
+    echo '|  ___/ _ \| \ | |  _ \| ____/ ___| / _ \ '
+    echo '| |_ | | | |  \| | | | |  _| \___ \| | | |'
+    echo '|  _|| |_| | |\  | |_| | |___ ___) | |_| |'
+    echo '|_|   \___/|_| \_|____/|_____|____/ \___/ '
+    echo
     echo 'Instalación finalizada'
     exit 0
 else
-    # echo 'Este script debe ejecutarse como root.' >&2
     echo ' _____ ___  _   _ ____  _____ ____   ___  '
     echo '|  ___/ _ \| \ | |  _ \| ____/ ___| / _ \ '
     echo '| |_ | | | |  \| | | | |  _| \___ \| | | |'
@@ -39,14 +44,23 @@ else
     echo '|_|   \___/|_| \_|____/|_____|____/ \___/ '
     echo
     echo 'Usuario normal detectado, inicia instalación...'
-    echo 'habilitando extensiones'
+    sleep 2
+    echo 'Descargando extensiones...'
     wget https://github.com/FONDESO/Soporte/raw/main/extensions.zip
     unzip extensions.zip -d ~/.local/share/gnome-shell/extensions
+    echo 'Habilitando extensiones...'
+    slep 2
     gnome-shell-extension-tool enable dash-to-dock@micxgx.gmail.com
     gnome-shell-extension-tool enable transparentosd@ipaq3870
     gnome-shell-extension-tool enable TransparentTopbar@enrico.sorio.net
     gnome-shell-extension-tool enable user-theme@gnome-shell-extensions.gcampax.github.com
     rm extensions.zip
+    echo ' _____ ___  _   _ ____  _____ ____   ___  '
+    echo '|  ___/ _ \| \ | |  _ \| ____/ ___| / _ \ '
+    echo '| |_ | | | |  \| | | | |  _| \___ \| | | |'
+    echo '|  _|| |_| | |\  | |_| | |___ ___) | |_| |'
+    echo '|_|   \___/|_| \_|____/|_____|____/ \___/ '
+    echo
     echo 'Instalación finalizada'
     exit 1
 fi
